f0b17e3629eb019726e82d80d760c6cea1488faa
Victor Santos <victor.eas19@gmail.com>
2023-09-04 19:30:59 -0300

    array done

--- todolist.ts
+++ todolist.ts
@@ -1,26 +1,29 @@
 class Tarefa {
-    #descricao: string;
-    #concluida: boolean;
+    descricao: string;
+    concluida: boolean;
     constructor(descricao: string, concluida: boolean) {
-        this.#descricao = descricao;
-        this.#concluida = concluida;
+        this.descricao = descricao;
+        this.concluida = concluida;
     };
 
-    public set setConcluida(concluida: boolean) {
-        this.#concluida = concluida;
+    /*public set setConcluida(concluida: boolean) {
+        this.concluida = concluida;
     }
 
     public get getDescricao(): string {
-        return this.#concluida ? this.#descricao + ' (Concluída)' : this.#descricao + ' (Pendente)'
+        return this.concluida ? this.descricao + ' (Concluída)' : this.descricao + ' (Pendente)';
     }
     public set setDescricao(descricao: string){
-        this.#descricao = descricao;
-    }
+        this.descricao = descricao;
+    }*/
 }
 class ListaDeTarefas {
-    tarefas: Tarefa[] = [{descricao: 'lorem', concluida: false}];
+    tarefas: Tarefa[] = [];
     public adicionarTarefa(descricao:string){
-        this.tarefas.push(new Tarefa(descricao, false))
+        this.tarefas.push({
+            descricao: descricao,
+            concluida: false,
+        });
     }
 };
 /*class ListaPrioritaria extends ListaDeTarefas {
@@ -32,7 +35,9 @@ class ListaDeTarefas {
 
 const lista = new ListaDeTarefas;
 //const listaP = new ListaPrioritaria;
+lista.adicionarTarefa('teste');
+lista.adicionarTarefa('teste2');
 
-console.log(lista.adicionarTarefa('teste'));
+console.log(lista);
 
 
